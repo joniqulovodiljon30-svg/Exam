68138b8a974f726a8f7e0eed735e7bfd4d1a1a2f
Odilxon <joniqulovodiljon30@gmail.com>
2026-02-20 20:40:29 +0500

    Update App.tsx
    Signed-off-by: Odilxon <joniqulovodiljon30@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect, useMemo } from 'react';
 import { SECTIONS, QUESTIONS as RAW_QUESTIONS } from './data';
 import { Section, UserAnswer, AppView } from './types';
